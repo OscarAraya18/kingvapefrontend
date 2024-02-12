521a599f71a62533bb3469f14e5497781a1dd137
OscarAraya18 <osaronaragar15@gmail.com>
2024-02-12 09:56:42 -0600

    correccion descuento

--- src/views/app/apps/ranking.vue
+++ src/views/app/apps/ranking.vue
@@ -70,7 +70,7 @@ import axios from 'axios';
 import ApexCharts from 'apexcharts'
 
 const constants = require('@../../../src/constants.js'); 
-const webSocket = new WebSocket('wss:telasmasbackend.onrender.com');
+const webSocket = new WebSocket('wss:kingvapebackend2.onrender.com);
 
 export default {
   
