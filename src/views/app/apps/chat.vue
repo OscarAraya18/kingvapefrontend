b62d753d31b4116f02aff4619133d80a32e1e05f
Oscar Araya <oscararaya@Laptop-de-Oscar.local>
2024-10-21 13:30:22 -0600

    cambios

--- src/views/app/apps/chat.vue
+++ src/views/app/apps/chat.vue
@@ -798,7 +798,7 @@
 
             
             
-            <div style="background-image: url('https://i.postimg.cc/fTRJ9KCf/8c98994518b575bfd8c949e91d20548b.jpg');">
+            <div style="background-image: url('https://i.postimg.cc/j5ycYq59/1.jpg');">
               <div class="chat-content scroll" style="padding-bottom: 35px; overflow-y: scroll" ref="scrollRef">
                 <div v-for="currentActiveConversationMessage in currentActiveConversation.whatsappConversationMessages" @contextmenu.prevent="replyMessageRightClick(currentActiveConversationMessage)">
                   <div class="d-flex mb-30 appearMessageAnimation" :class="getMessageOwnerStyle(currentActiveConversationMessage.whatsappGeneralMessageOwnerPhoneNumber)">
