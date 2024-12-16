3c96d0a4496ce7dbc8484cf212801e23a2366a55
Oscar Araya <oscararaya@Laptop-de-Oscar.local>
2024-12-16 11:04:07 -0600

    cambios

--- src/views/app/apps/chat.vue
+++ src/views/app/apps/chat.vue
@@ -4064,7 +4064,7 @@ export default {
       } else if (regularExpressionChecker.test(this.currentActiveConversation.whatsappConversationRecipientNote) == false){
         this.showNotification('danger', 'Nota del envío incompleta', 'Por favor, seleccione una nota del envío para generar la orden, e intentelo nuevamente');
         return false;
-      } else if (this.currentActiveConversation.SINPEValidated != 'yes' && this.currentActiveConversation.whatsappInvoicePaymentMethod == 'SINPE (Confirmado)'){
+      } else if (this.currentActiveConversation.SINPEValidated != 'yes' && this.whatsappInvoicePaymentMethod == 'SINPE (confirmado)'){
         this.showNotification('danger', 'SINPE no confirmado', 'Por favor, valide el pago por SINPE e intentelo nuevamente');
         return false;
       }
