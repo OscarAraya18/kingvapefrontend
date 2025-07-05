2e34d8078cf046d760b2d11d4fef01648a2cfd9b
Oscar Araya <oscararaya@Laptop-de-Oscar.local>
2025-07-05 17:17:02 -0600

    cambios

--- src/views/app/apps/rankingadmin.vue
+++ src/views/app/apps/rankingadmin.vue
@@ -624,6 +624,7 @@ export default {
       var agentColors = [];
       const colorByAgent = 
       {
+        'Ashley Quiros': '#ffa578',
         'Julissa Chavarria': '#2dad9e',
         'Sergio Barrot': '#424880',
         'Jose Estrada': '#255c34',
